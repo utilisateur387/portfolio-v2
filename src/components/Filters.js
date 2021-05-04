f86d8ae97eaf1d9be3654a5ef468c941373d851a
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-04 22:36:34 +0200

    Add smooth scroll to anchor

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react';
+
 const Filters = () => {
+  const [anchor, setAnchor] = useState(null);
+  useEffect(() => {
+    setAnchor(document.getElementById('filters'))
+  }, [])
+
+  console.log(anchor);
+  const handleClick = () => {
+    anchor.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div id="filters" >
+      <button onClick={handleClick}>Test</button>
       <hr/>
     </div>
   )
 }
 
 export default Filters;
+
--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
 
 const Headline = () => {
+  const anchor = document.getElementById('else');
+  console.log(anchor);
+  const handleClick = (e) => {
+    e.preventDefault();
+    anchor.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <div className="headline">
       <p className="headlines">
         Hi, I’m Miebi, a web developer and digital designer currently based in France
       </p>
-{      <div className="links">
-        <a href="#filters">Work</a>
+      <div className="links">
+        <a href="#filters" onClick={handleClick}>Work</a>
         <Link to="/about">About Me</Link>
-      </div>}
+      </div>
     </div>
   )
 }
