73cc82feba6d4ffb226de15baa039c5bb6eac04e
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-04 21:36:24 +0200

    Fix navbar and clean folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import About from './About';
-import ProjectDetails from './ProjectDetails';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import ProjectDetails from './components/ProjectDetails';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">Work</Link>
-      <Link to="/about">About</Link>
+      <div>
+        <Link to="/">Work</Link>
+        <Link to="/about">About</Link>
+      </div>
     </div>
   )
 }
