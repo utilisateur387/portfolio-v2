aed27fb52dc1b162331fee86f849514f11c3db37
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-07 17:53:39 +0200

    Add cross for filters module

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -28,6 +28,7 @@ const FilterButton = ({ tags, toggleFilters, tagCount }) => {
         >
         <span className="filter-title" onClick={handleClick}>Filters ({tagCount})</span>
         <div style={{ width: '90vw' }}>
+          <img class="img-cross-block" src="images/general/cross.png" alt="" />
           <Tags tagsDisplay={tagsDisplay} tags={tags} toggleFilters={toggleFilters} />
         </div>
       </motion.div>
