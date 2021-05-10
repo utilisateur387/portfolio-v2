4a52c20f5ccddc84ebd96c8168e986c483bcbe11
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-10 22:28:59 +0200

    Add back arrow

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,24 @@ const About = ({ toggleAbout, showAbout }) => {
         animate={{ y: 0 }}
         transition={{ duration: 0.7, stiffness: 30 }}
         exit={{ y: '100vh' }}>
-        <div onClick={toggleAbout}><u>Home</u></div>
+        <div class="about-close">
+
+          <img
+            src="images/general/cross.png" alt=""
+            onClick={toggleAbout}
+            class="img-cross-block cross-about"
+            />
+        </div>
+
+        <div class="about-back" onClick={toggleAbout}>
+          <img src="images/general/arrow_left.png" alt="" className="arrow-back-about" />
+          Back
+        </div>
+
         <p>
           I'm an arts and culture enthusiast, especially when it comes to new technologies and immersive experiences. After working as a graphic designer in New York, I joined Le Wagon coding bootcamp to become a web developer and extend my skills in the digital field.
         </p>
+
         <div className="about-content">
           <div>
             Hello
@@ -22,11 +36,13 @@ const About = ({ toggleAbout, showAbout }) => {
             Hello
           </div>
         </div>
+
         <div className="about-links">
           Github,
           LinkedIn,
           Instagram
         </div>
+
       </motion.div> )}
     </AnimatePresence>
 
